6c10cbc046e3e0cab0b380dc42e0ce92a975542e
Gabriel Antonio <gmav1010@gmail.com>
2020-05-19 08:06:26 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -6,11 +6,21 @@ int main() {
 	cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
 	cout << "--------------------------------" << endl;
 	cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
+	//ler volume
 	int compras;
 	double vmedio;
 	cout << endl << "Quantas compras o cliente fez no ultimo ano? ";
 	cin >> compras;
 	cout << "Qual o ticket medio? ";
 	cin >> vmedio;
+	cout << endl;
+	//ler outros
+	int atraso;
+	string pagamento;
+	cout << "Quantas vezes o cliente atrasou o pagamento? ";
+	cin >> atraso;
+	cout << "A maioria das compras foi em dinheiro, cartão, ou boleto (D/C/B)? ";
+	cin >> pagamento;
+	
 	return 0;
 }
