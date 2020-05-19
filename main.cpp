cfc72bd75f020b78a8242294aab81e917afb2353
Gabriel Antonio <gmav1010@gmail.com>
2020-05-19 07:59:10 -0300

    Leitura do volume

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ int main() {
 	cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
 	cout << "--------------------------------" << endl;
 	cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
-	
+	int compras;
+	double vmedio;
+	cout << endl << "Quantas compras o cliente fez no ultimo ano? ";
+	cin >> compras;
+	cout << "Qual o ticket medio? ";
+	cin >> vmedio;
 	return 0;
 }
